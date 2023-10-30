6c3e3552570ea55ba8dcb7eca7d585358814b2cf
tranguyen <tranguyen.contact@gmail.com>
2023-10-30 14:36:44 +0700

    Add comment

--- Lab02/AimsProject/src/Cart.java
+++ Lab02/AimsProject/src/Cart.java
@@ -1,18 +1,21 @@
 public class Cart {
-    private int qtyOrdered;
+    private int qtyOrdered; //Quantity of items in cart
     public static final int MAX_NUMBERS_ORDERED = 20;
-    private DigitalVideoDisc itemsOrdered[] =
+    private final DigitalVideoDisc[] itemsOrdered =
             new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-    public void addDigitalVideoDisc(DigitalVideoDisc disc){
-        if (qtyOrdered < MAX_NUMBERS_ORDERED ){
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){ // add a DVD to cart
+        if (qtyOrdered < MAX_NUMBERS_ORDERED ){ // check quantity of cart
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
+            // notification in console when add successfully
             System.out.println("The disc has been added.");
         }
         else{
+            //notification in console when cart is full
             System.out.println("The cart is almost full. Cannot add more discs.");
         }
     }
+    //remove DVD method
     public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
         for (int i = 0; i < qtyOrdered; i++) {
             if (itemsOrdered[i] == disc) {
@@ -27,7 +30,7 @@ public class Cart {
         }
         System.out.println("The disc is not in the cart.");
     }
-    public float totalCost() {
+    public float totalCost() { //total cost calc
         float total = 0;
         for (int i = 0; i < qtyOrdered; i++) {
             total += itemsOrdered[i].getCost();
--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -1,6 +1,6 @@
 public class DigitalVideoDisc {
 
-    private String title;
+    private final String title;
     private String category;
     private String director;
     private int length;
@@ -20,6 +20,7 @@ public class DigitalVideoDisc {
     public float getCost() {
         return cost;
     }
+    //add DVD with full information
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
         this.category = category;
@@ -27,15 +28,18 @@ public class DigitalVideoDisc {
         this.length = length;
         this.cost = cost;
     }
+    //add DVD with only title
     public DigitalVideoDisc(String title) {
         super();
         this.title = title;
     }
+    //add DVD with category, title and cost
     public DigitalVideoDisc(String category, String title, float cost) {
         this.title = title;
         this.category = category;
         this.cost = cost;
     }
+    //add DVD with director, category, title and cost
     public DigitalVideoDisc( String director, String category, String title,  float cost) {
         this.title = title;
         this.category = category;
