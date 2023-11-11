5964fadc06f49c38fa8b0988ca71d22cbb78d8c9
tranguyen <tranguyen.contact@gmail.com>
2023-11-11 11:14:20 +0700

    overloading method

--- Lab03/AimsProject/src/TestOverloading.java
+++ Lab03/AimsProject/src/TestOverloading.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class TestOverloading {
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class TestOverloading {
+    public static void main(String[] args) {
+        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation",
+                "Roger Allers", 87, 19.95f);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Start Wars", "Science Fiction",
+                "George Lucas", 87, 24.95f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation",
+                18.99f);
+
+        DigitalVideoDisc[] dvdList = new DigitalVideoDisc[]{dvd1, dvd2, dvd3};
+        anOrder.addDigitalVideoDisc(dvdList);
+        System.out.println("Total Cost is: " + anOrder.totalCost());
+    }
 }
