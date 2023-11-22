64cc0377459bfdb0f11e2df2560ab35edd8d1592
Nguyễn Anh Trà <132732319+anhtranguyen-github@users.noreply.github.com>
2023-11-22 23:56:52 +0700

    Delete Lab01 directory

--- Lab01/2.2.1-2.2.6/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-/*Nguyen_Anh_Tra_20215151 */
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Xin chao\n cac ban!");
-        System.out.println("Hello\nWorld!");
-    }
-}
