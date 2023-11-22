169ba2114034e471c5a0decab11b535bb5817382
tranguyen <tranguyen.contact@gmail.com>
2023-11-23 00:01:03 +0700

    reupload

---
+++ Lab03/AimsProject/src/hust/soict/ite6/test/store/StoreTest.java
@@ -0,0 +1,19 @@
+// Nguyen Anh Tra - 20215151
+package hust.soict.ite6.test.store;
+
+import hust.soict.ite6.aims.disc.DigitalVideoDisc;
+import hust.soict.ite6.aims.store.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("DVD 1");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("DVD 2");
+        Store store = new Store();
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+
+        store.removeDVD(dvd1);
+    }
+}
+
+
