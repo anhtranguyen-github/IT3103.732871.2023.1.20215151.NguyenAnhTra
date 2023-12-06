14ec5c8fd4b57251bfc68d98d5b77bb554d3fccb
tranguyen <tranguyen.contact@gmail.com>
2023-12-06 21:41:32 +0700

    add playable interface

--- Lab04/AimsProject/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
+++ Lab04/AimsProject/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
@@ -2,80 +2,81 @@
 
 package hust.soict.ite6.aims.media;
 
-public class DigitalVideoDisc extends Media{
-
-    private String title;
-    private String category;
+public class DigitalVideoDisc extends Media implements Playable{
     private String director;
     private int length;
-    private float cost;
-    private static int nbDigitalVideoDiscs = 0;
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getCategory() {
-        return category;
-    }
+    private static int nbDigitalVideoDisc = 0;
+
     public String getDirector() {
         return director;
     }
     public int getLength() {
         return length;
     }
-    public float getCost() {
-        return cost;
-    }
-    //init method for nbDigitalVideoDiscs
-    private void init() {
-        nbDigitalVideoDiscs++;
-    }
-    //get nbDigitalVideoDiscs
-    public static int getQtyDvd(){
-        return nbDigitalVideoDiscs;
+    public DigitalVideoDisc(String title) {
+        super();
+        this.title = title;
     }
 
-    //add DVD with full information
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super();
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
-        init();
     }
-    //add DVD with only title
-    public DigitalVideoDisc(String title) {
-        super();
-        this.title = title;
-        init();
+
+    /**
+     * The function takes the information stored in the object and returns it as a string
+     *
+     * @return A string representation of the DVD object.
+     */
+    public String toString(){
+        StringBuffer printDisc = new StringBuffer();
+        printDisc.append("DVD");
+        printDisc.append(" - ");
+        printDisc.append(title);
+        printDisc.append(" - ");
+        printDisc.append(category);
+        printDisc.append(" - ");
+        printDisc.append(director);
+        printDisc.append(" - ");
+        printDisc.append(length);
+        printDisc.append(": ");
+        printDisc.append(cost);
+        return  printDisc.toString();
     }
-    //add DVD with category, title and cost
-    public DigitalVideoDisc(String category, String title, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-        init();
-    }
-    //add DVD with director, category, title and cost
-    public DigitalVideoDisc( String director, String category, String title,  float cost) {
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
-        init();
-    }
-    public boolean isMatch(String title) {
-        return this.title.equalsIgnoreCase(title);
+
+    /**
+     * This function returns true if the title of the book is equal to the title passed in as a parameter.
+     *
+     * @param title The title of the page.
+     * @return The boolean value of the comparison of the title of the book to the title of the book being searched for.
+     */
+    public boolean isMatch(String title){
+        return this.title.equals(title);
     }
 
     @Override
-    public String toString() {
-        return String.format("DVD - %-20s - %-15s - %-20s - %-5d - %-5.2f$",
-                title, category, director, length, cost);
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
     }
-
-
 }
